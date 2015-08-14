0faca8bbe5707836d793ca2e736d2ef1550d0904
Nate Derbinsky <nate.derbinsky@gmail.com>
2015-08-14 12:06:52 -0400

    Added optional shortcut timecode commands for slot numbers

--- latex/schedule/week.php
+++ latex/schedule/week.php
@@ -18,6 +18,42 @@
 	
 	echo "\n\n\n";
 	
+	for ( $hour=$start_h, $slot=1; $hour<=$end_h; $hour++ )
+	{
+		$real_hour = $hour;
+		$ampm = 'am';
+		if ( $real_hour >= 12 )
+		{
+			$ampm = 'pm';
+			
+			if ( $real_hour > 12 )
+			{
+				$real_hour -= 12;
+			}
+		}
+		
+		$rn = '';
+		if ( $real_hour == 1 ) $rn = 'I';
+		if ( $real_hour == 2 ) $rn = 'II';
+		if ( $real_hour == 3 ) $rn = 'III';
+		if ( $real_hour == 4 ) $rn = 'IV';
+		if ( $real_hour == 5 ) $rn = 'V';
+		if ( $real_hour == 6 ) $rn = 'VI';
+		if ( $real_hour == 7 ) $rn = 'VII';
+		if ( $real_hour == 8 ) $rn = 'VIII';
+		if ( $real_hour == 9 ) $rn = 'IX';
+		if ( $real_hour == 10 ) $rn = 'X';
+		if ( $real_hour == 11 ) $rn = 'XI';
+		if ( $real_hour == 12 ) $rn = 'XII';
+		
+		for ( $half=0; $half<=1; $half++, $slot++ )
+		{
+			echo '\newcommand{\tc' . $rn . $ampm . ( ( $half == 0 )?(''):('H') ) . '}{' . $slot . '}' . "\n";
+		}
+	}
+	
+	echo "\n\n\n";
+	
 	foreach ( $days as $day )
 	{
 		for ( $slot=1; $slot<=$num_slots; $slot++ )
